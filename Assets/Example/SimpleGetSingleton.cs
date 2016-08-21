e5c0112444304eb5e2145b752befa756c13fab01
OpenLevel <thdwlgnsdl10@naver.com>
2016-08-21 10:14:23 +0900

    Add a singleton example

---
+++ Assets/Example/SimpleGetSingleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+namespace OpenLevel
+{
+    public class SimpleGetSingleton : MonoBehaviour
+    {
+        public string uri = "https://raw.githubusercontent.com/OpenLevel/UnityWebClient/master/README.md";
+        UnityWebClient _webClient;
+
+        // Use this for initialization
+        void Start()
+        {
+            _webClient = UnityWebClientSingleton.Instance;
+            _webClient.Get(uri, HandleResponse);
+        }
+
+        void HandleResponse(UnityWebRequest request)
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
+    }
+}
